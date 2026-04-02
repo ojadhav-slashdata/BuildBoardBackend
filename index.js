d9ae9363ac042fb40a90d579ee059ce447bf76be
BuildBoard Team <buildboard@hackathon.dev>
2026-04-02 17:13:07 +0400

    fix: increase JSON body limit to 4MB for base64 file attachments

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { runMigrations, getMigrationSQL } = require('./migrate');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '4mb' }));
 
 // Run migrations on startup
 runMigrations().catch(err => console.error('[migrate] Failed:', err.message));
