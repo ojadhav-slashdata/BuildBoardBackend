0d24d0c62d58d721436e37cb9de74fd898a45d14
BuildBoard Team <buildboard@hackathon.dev>
2026-04-02 15:07:43 +0400

    Revert "feat: notifications API endpoint + demo seed data"
    This reverts commit 7cb8f9a01a24b7200984514891777bfc2b3c1515.

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const analyticsRoutes = require('./routes/analytics');
 const pointsRoutes = require('./routes/points');
 const rewardsRoutes = require('./routes/rewards');
 const projectsRoutes = require('./routes/projects');
-const notificationsRoutes = require('./routes/notifications');
 
 const { runMigrations, getMigrationSQL } = require('./migrate');
 
@@ -52,7 +51,6 @@ app.use('/api', pointsRoutes);
 app.use('/api/rewards', rewardsRoutes);
 app.use('/api/bids', require('./routes/bidActions'));
 app.use('/api/projects', projectsRoutes);
-app.use('/api/notifications', notificationsRoutes);
 
 const swaggerOptions = {
   customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css',
